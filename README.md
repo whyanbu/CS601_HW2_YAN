e2c8bd5206eabe2cc410b00c042c3f07f05084e6
whyanbu <whyan@bu.edu>
2025-11-10 01:02:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Identify Fruits and Vegetables
+A webpage for testing classification of fruits and vegetables.  Drag and drop all the fruits and vegetables to their respective drop area.  If matched, the element will added to the drop area and turn to light sky blue.  If unmatch, the element will stay in the original list.  Result will also be shown at the bottom of the webpage.  
+
+## Table of Contents
+Features  
+Installation  
+Usage  
+Project Structure  
+
+### Features
+Written with vanilla JavaScript without CSS frameworks.  
+JSON file is hosted in github page: https://whyanbu.github.io/CS601_Project/public/cs601-assignment2-data.json  
+
+Alternatively, similar data is stored in Firebase, and an Express.js API, deployed on Vercel, exposes the data as a JSON return via an HTTP GET endpoint:  
+https://cs601projectapi.vercel.app/get  
+
+
+### Installation
+Open index.html directly in your web browser.  
+No build or dependencies needed.  
+
+### Usage
+All application logic is in the /js directory.  
+
+Start the app by opening index.html.  
+
+### Project Structure
+```
+CS601_HW2_YAN/  
+│  
+├── js/  
+│   └── main.js  
+├── css/  
+│   └── main.css  
+├── index.html
+└── README.md  
+```
+index.html: Entry point for the app.  
---
+++ js/main.js
@@ -0,0 +1,64 @@
+const dataList = document.getElementById("data-list");
+const dropAreas = document.querySelectorAll(".drop-area");
+const result = document.getElementById("result");
+
+const url = "https://whyanbu.github.io/CS601_Project/public/cs601-assignment2-data.json";
+// const url = "https://cs601projectapi.vercel.app/get";
+
+const fetchData = async () => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+        for (d of data) {
+            const li = document.createElement("li");
+            li.draggable = true;
+            li.setAttribute("data-item-id", d.id);
+            li.setAttribute("data-name", d.name);
+            li.setAttribute("data-category", d.category);
+            li.innerText = d.name;
+            dataList.appendChild(li);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+const initialize = async () => {
+    await fetchData();
+    dataList.querySelectorAll("li").forEach(record => record.addEventListener("dragstart", e => {
+        const selected = e.target;
+        e.dataTransfer.setData("application/json", JSON.stringify(
+            {
+                id: selected.getAttribute("data-item-id"),
+                name: selected.getAttribute("data-name"),
+                category: selected.getAttribute("data-category")
+            }
+        ));
+    }));
+}
+
+dropAreas.forEach(da => da.addEventListener("dragover", e => {
+    e.preventDefault();
+    e.currentTarget.classList.add("drag-over");
+}));
+
+dropAreas.forEach(da => da.addEventListener("dragleave", e => {
+    e.currentTarget.classList.remove("drag-over");
+}));
+
+dropAreas.forEach(da => da.addEventListener("drop", e => {
+    e.currentTarget.classList.remove("drag-over");
+    const draggedData = JSON.parse(e.dataTransfer.getData("application/json"));
+    const dom = document.querySelector(`[data-item-id="${draggedData.id}"]`);
+    if (draggedData.category === da.getAttribute("data-category")) {
+        da.appendChild(dom);
+        dom.classList.add("correct");
+        dom.draggable = false;
+        result.innerText = `CORRECT.  ${draggedData.name} is ${da.getAttribute("data-category")}.`;
+    } else {
+        result.innerText = `INCORRECT.  ${draggedData.name} is NOT ${da.getAttribute("data-category")}.`;
+    }
+}));
+
+initialize();
